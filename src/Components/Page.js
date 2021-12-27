e46b58490e5e02839b93f00329b5bb309d4ec6d9
mohamed baduan <mohamedabbj@gmail.com>
2021-12-27 19:10:48 -0400

    Added some CSS and Date to Page

--- src/Components/Page.js
+++ src/Components/Page.js
@@ -23,11 +23,11 @@ import "../Styles/weather.css";
             countryBox +
             "&units=metric&appid=c916e991cc31e02e0ab1b62115ef3e8f"
         );
-        const response2 = await axios.get("https://timeapi.io/api/Time/current/coordinate?latitude=8&longitude=-66")
+        /*const response2 = await axios.get("https://timeapi.io/api/Time/current/coordinate?latitude=8&longitude=-66")
         response2.header("Access-Control-Allow-Origin", "*");
         response2.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");  
+        console.log(response2)*/
         console.log(response)
-        console.log(response2)
         setCountryData({
           countryName: response.data.name,
           countryTemperature: Math.round(response.data.main.temp),
@@ -56,6 +56,15 @@ import "../Styles/weather.css";
       );
     };
   
+    let WeatherExtraElements = () =>{
+      return (
+        <div className="weatherExtras">
+          <p>Date</p>
+          <p>dsads</p>
+        </div>
+      )
+    }
+
     let WeatherElements = () => {
       return (
         <>
@@ -89,6 +98,10 @@ import "../Styles/weather.css";
   
         <div className="elements">
           <WeatherElements />
+        </div>
+
+        <div>
+          <WeatherExtraElements/>
         </div>
         
       </>
