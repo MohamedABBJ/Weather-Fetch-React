f957341918aed58bdf14ebdc3a95023d00fde9d9
MohamedABBJ <MohamedABBJ>
2022-01-07 20:58:56 -0400

    Fixing some code

--- src/index.js
+++ src/index.js
@@ -3,13 +3,4 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Page from "./Components/FetchingData.js";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-          <Route path="page" element={<Page />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Page />, document.getElementById("root"));
