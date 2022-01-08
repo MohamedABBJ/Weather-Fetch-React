3bd85a6dcc1371a464a3b7e76e0ed191a48656e5
MohamedABBJ <MohamedABBJ>
2022-01-08 19:36:58 -0400

    Refactoring WeatherElements.js code

--- src/Components/WeatherElements.js
+++ src/Components/WeatherElements.js
@@ -1,80 +1,76 @@
-import React, { useEffect, useRef, useState } from "react";
 import "../Styles/weather.css";
 import CloudyIcon from "../Styles/Assets/cloudy-day-1.svg";
 import ClearDayIcon from "../Styles/Assets/day.svg";
 import RainyIcon from "../Styles/Assets/rainy-7.svg";
 import SnowyIcon from "../Styles/Assets/snowy-6.svg";
 
-let WeatherElements = (props) => {
+const API_URL = "http://openweathermap.org"
 
-  if (props.countryTimeV !== undefined) {
-    var countryTimeSunsetDef = props.countryTimeSunsetV;
-    var countryTimeSunriseDef = props.countryTimeSunriseV;
-    var countryTimeDef = props.countryTimeV
-    var temperature = "Temperature";
-    var weatherType = "Weather Type";
-    var weatherIcon =
-      "http://openweathermap.org/img/wn/" +
-      props.countryWeatherIconV +
-      "@2x.png";
-    var countryTemperatureDef = props.countryTempV
-    var temperatureInCelsius = "°C"
-    var sunsetTime = new Date(countryTimeSunsetDef * 1000).toLocaleTimeString();
-    var sunriseTime = new Date(
-      countryTimeSunriseDef * 1000
-    ).toLocaleTimeString();
-    var sunset = "Sunset";
-    var sunrise = "Sunrise";
-    var firstMessageRender = "removeFirstMessage";
-    var weatherTypeData = props.countryWeatherTypeV;
-    var timer = true
-    
-    } else {
-    weatherTypeData = "";
-    sunset = "";
-    sunrise = "";
-    countryTimeSunsetDef = "";
-    countryTimeSunriseDef = "";
-    temperature = "";
-    weatherType = "";
-    weatherIcon = "";
-    temperatureInCelsius = ""
-    countryTemperatureDef = ""
-    firstMessageRender = "firstMessage";
-  }
+
+let WeatherElements = (props) => {
+  const countryTemperatureDef = props.countryTempV
+  const countryTimeSunriseDef = props.countryTimeSunriseV;
+  const countryTimeSunsetDef = props.countryTimeSunsetV;
+  const sunsetTime = new Date(countryTimeSunsetDef * 1000).toLocaleTimeString();
+  const sunriseTime = new Date(countryTimeSunriseDef * 1000).toLocaleTimeString();
   
-  if (weatherTypeData === "Clouds") {
-    weatherTypeData = "Cloudy";
-    weatherIcon = CloudyIcon;
-  } else if (weatherTypeData === "Clear") {
-    weatherIcon = ClearDayIcon;
-  } else if (weatherTypeData === "Snow") {
-    weatherTypeData = "Snowy";
-    weatherIcon = SnowyIcon;
-  } else if (weatherTypeData === "Rain") {
-    weatherTypeData = "Rainy";
-    weatherIcon = RainyIcon;
+  
+  const getIcon = () =>{
+    return(
+      `${API_URL}/img/wn/${props.countryWeatherIconV}@2x.png`
+      )}
+      
+  let weatherIcon = getIcon()
+  let weatherTypeData = props.countryWeatherTypeV;
+  
+    
+  
+  switch (weatherTypeData){
+    case `Clouds`:
+      weatherTypeData = `Cloudy`
+      weatherIcon = CloudyIcon;
+      break
+    case `Clear`:
+      weatherIcon = ClearDayIcon;
+      break
+    case `Snow`:
+      weatherTypeData = `Snowy`;
+      weatherIcon = SnowyIcon;
+      break
+    case `Rain`:
+      weatherTypeData = "Rainy";
+      weatherIcon = RainyIcon;
+      break
+    default:
+  }
+
+  if(props.countryTimeV === undefined){
+    return(
+      <div>
+        {console.log("Probando")}
+      </div>
+    )
   }
 
   return (
     <> 
-      <h2>{temperature}</h2>
-      <h2 id="temp">{countryTemperatureDef + "" + temperatureInCelsius}</h2>
+      <h2>{`Temperature`}</h2>
+      <h2 id="temp">{countryTemperatureDef  + `°C`}</h2>
     
       <div className="weatherType">
-        <h2>{weatherType}</h2>
+        <h2>{`Weather Type`}</h2>
         <img src={weatherIcon} width={100} alt="" />
         <h2 id="weather">{weatherTypeData}</h2>
       </div>
 
       <div className="weatherSunsetSunrise">
-        <p>{sunset}</p>
+        <p>{`Sunset`}</p>
         <p>{sunsetTime}</p>
-        <p>{sunrise}</p>
+        <p>{`Sunrise`}</p>
         <p>{sunriseTime}</p>
       </div>
 
-      <div className={firstMessageRender}>
+      <div className={`removeFirstMessage`}>
         <h1>Write a country or location to make a search</h1>
         <h1>Format (city name or country) + , + 2 letter country code</h1>
         <h1>Example: Florida, US</h1>
