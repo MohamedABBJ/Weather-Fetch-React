0c6f779305d3d06cbcd9a0c17691b9be6c75fa06
MohamedABBJ <MohamedABBJ>
2022-01-11 22:20:35 -0400

    Refactor WeatherElements2.js

--- src/Components/WeatherElements2.js
+++ src/Components/WeatherElements2.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useMemo, } from 'react'
 import "../Styles/weather.css";
 
 let WeatherElements2 = (props) =>{
@@ -8,71 +8,79 @@ let WeatherElements2 = (props) =>{
   const [timeState, settimeState] = useState("hideTime")
   const [timeAmPm, settimeAmPm] = useState("")
 
+  const obtainData = {
+    timer : false,
+    countryTime : props.countryTimeV,
+    countryName : props.countryNameV,
+    countryNameAbbr : props.countryNameAbvV,
+    countryDate : props.countryDateV,
+    countryDayOfWeek : props.countryDayOfWeekV,
+  }
 
-    if (props.countryTimeV !== undefined) {
-        var countryTimeDef = props.countryTimeV;
-        var countryNameDef = props.countryNameV;
-        var countryNameAbvDef = props.countryNameAbvV;
-        var countryDateDef = props.countryDateV;
-        var countryDayOfWeekDef = props.countryDayOfWeekV;
-        var countryTimeDefS = parseInt(countryTimeDef.slice(6,8))
-        var countryTimeDefM = parseInt(countryTimeDef.slice(3,5))
-        var countryTimeDefH = parseInt(countryTimeDef.slice(0,2))
-        var countryTimeDefPmAm = countryTimeDef.slice(8,11)
-        var timer = true
-    }
-        else{
-        countryTimeDef = "";
-        countryDateDef = "";
-        countryNameDef = "";
-        countryNameAbvDef = "";
-        countryDayOfWeekDef = "";
-    }
+  if(props.countryTimeV === undefined){
+    obtainData.countryTime = "0"
+  }
 
+  if(props.countryTimeV !== undefined){
+    obtainData.timer = true
+  }
 
-    const firstUpdate = useRef(true)
-
+  const timeFormat = useMemo(() => new Map([
+      ["TimeSeconds", parseInt(obtainData.countryTime.slice(6,8))],
+      ["TimeMinutes", parseInt(obtainData.countryTime.slice(3,5))],
+      ["TimeHours",parseInt(obtainData.countryTime.slice(0,2))],
+      ["TimeAmPm", obtainData.countryTime.slice(8,11)],
+    ]), [obtainData.countryTime])
+     
     useEffect(() => {
-      seteverySecond(countryTimeDefS)
-      seteveryMinute(countryTimeDefM)
-      seteveryHour(countryTimeDefH)
-      settimeAmPm(countryTimeDefPmAm)
-    }, [countryTimeDef , countryTimeDefS, countryTimeDefPmAm, countryTimeDefH, countryTimeDefM])
+      seteverySecond(timeFormat.get("TimeSeconds"))
+      seteveryMinute(timeFormat.get("TimeMinutes"))
+      seteveryHour(timeFormat.get("TimeHours"))
+      settimeAmPm(timeFormat.get("TimeAmPm"))
+    }, [timeFormat])  
   
     useEffect(() => {
-  
-      if (firstUpdate.current) {
-        firstUpdate.current = false
-        return
-      }
+
       settimeState("showTime")
-      if(timer === true){
-          setInterval(() => {
-            seteverySecond(
-              everySecond => (everySecond + 1)
-            )
-          }, 1000);
-      }
-    }, [timer])
+
+         if(obtainData.timer === true){
+           setInterval(() => {
+             seteverySecond(
+               everySecond => (everySecond + 1)
+             )
+           }, 1000);
+         }
+      
+    }, [obtainData.timer])
     
   
     if(everySecond >= 60){
       seteverySecond(0)
       seteveryMinute(countryTimeDefM => countryTimeDefM + 1)
-    }else if(everyMinute >= 60){
+    }
+    if(everyMinute >= 60){
       console.log("a")
       seteveryMinute(0)
       seteveryHour(countryTimeDefH => countryTimeDefH + 1)
-    }else if (everyHour > 12 && timeAmPm === "AM"){
+    }
+    if (everyHour > 12 && timeAmPm === "AM"){
       seteveryHour(countryTimeDefH => countryTimeDefH = 1)
+    }
+
+
+    if (props.countryTimeV === undefined){
+      return(
+       <div>{}</div>
+      )
     }
 
     return(
         <div className="weatherExtras">
-        <h1>{countryNameDef + " " + countryNameAbvDef}</h1>
-        <p id="Date">{countryDayOfWeekDef + " " + countryDateDef}</p>
+        <h1>{obtainData.countryName + " " + obtainData.countryNameAbbr}</h1>
+        <p id="Date">{obtainData.countryDayOfWeek + " " + obtainData.countryDate}</p>
         <p id={timeState}>{everyHour + ":"+ everyMinute + ":" + everySecond + " " + timeAmPm}</p>
       </div>
     )
 }
+
 export default WeatherElements2
