56eeff52d3873070ae84c03f7a3a91181c2f0ac6
MohamedABBJ <MohamedABBJ>
2022-02-21 19:14:08 -0400

    Bug fixes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Page from "./Components/FetchingData.js";
 
 ReactDOM.render(<Page />, document.getElementById("root"));
